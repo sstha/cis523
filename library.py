cb2142fde636bddf8bbae2d224342f0f045925be
sstha <34565072+sstha@users.noreply.github.com>
2022-10-13 19:38:03 -0700

    Update library.py

--- library.py
+++ library.py
@@ -51,36 +51,32 @@ class DropColumnsTransformer(BaseEstimator, TransformerMixin):
     result = self.transform(X)
     return result
   
- class MappingTransformer(BaseEstimator, TransformerMixin):
-
-  def __init__(self, mapping_column, mapping_dict:dict):
-    assert isinstance(mapping_dict, dict), f'{self.__class__.__name__} constructor expected dictionary but got {type(mapping_dict)} instead.'
-    self.mapping_dict = mapping_dict
-    self.mapping_column = mapping_column  #column to focus on
-
-  def fit(self, X, y = None):
-    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
-    return X
-
-  def transform(self, X):
-    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
-    assert self.mapping_column in X.columns.to_list(), f'{self.__class__.__name__}.transform unknown column "{self.mapping_column}"'  #column legit?
-
+class MappingTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, mapping_column, mapping_dict:dict):
+        assert isinstance(mapping_dict, dict), f'{self.__class__.__name__} constructor expected dictionary but got {type(mapping_dict)} instead.' #f'{self.__class__.__name__} gets class name
+        self.mapping_dict = mapping_dict
+        self.mapping_column = mapping_column  #column to focus on
+    def fit(self, X, y = None):
+        print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+        return X
+    def transform(self, X):
+        assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+        assert self.mapping_column in X.columns.to_list(), f'{self.__class__.__name__}.transform unknown column "{self.mapping_column}"'  #column legit?
     #now check to see if all keys are contained in column
-    column_set = set(X[self.mapping_column])
-    keys_not_found = set(self.mapping_dict.keys()) - column_set
-    if keys_not_found:
-      print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] does not contain these keys as values {keys_not_found}\n")
+        column_set = set(X[self.mapping_column])
+        keys_not_found = set(self.mapping_dict.keys()) - column_set
+        if keys_not_found:
+            print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] does not contain these keys as values {keys_not_found}\n")
 
     #now check to see if some keys are absent
-    keys_absent = column_set -  set(self.mapping_dict.keys())
-    if keys_absent:
-      print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] does not contain keys for these values {keys_absent}\n")
+        keys_absent = column_set -  set(self.mapping_dict.keys())
+        if keys_absent:
+            print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] does not contain keys for these values {keys_absent}\n")
 
-    X_ = X.copy()
-    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
-    return X_
+        X_ = X.copy()
+        X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+        return X_
 
-  def fit_transform(self, X, y = None):
-    result = self.transform(X)
-    return result
+    def fit_transform(self, X, y = None):
+        result = self.transform(X)
+        return result
