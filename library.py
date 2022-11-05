69549070456634ebeb48ee53d258a5abb3517376
sstha <34565072+sstha@users.noreply.github.com>
2022-11-04 21:56:07 -0700

    Update library.py

--- library.py
+++ library.py
@@ -257,5 +257,26 @@ customer_transformer = Pipeline(steps=[
 def customer_setup(customer_table, transformer=customer_transformer, rs=76, ts=.2):
   label='Rating'
   return dataset_setup(customer_table, label, transformer, rs, ts)
+
+def threshold_results(thresh_list, actuals, predicted):
+  result_df = pd.DataFrame(columns=['threshold', 'precision', 'recall', 'f1', 'accuracy'])
+  for t in thresh_list:
+    yhat = [1 if v >=t else 0 for v in predicted]
+    precision = precision_score(actuals, yhat, zero_division=0)
+    recall = recall_score(actuals, yhat, zero_division=0)
+    f1 = f1_score(actuals, yhat)
+    accuracy = accuracy_score(actuals, yhat)
+    result_df.loc[len(result_df)] = {'threshold':t, 'precision':precision, 'recall':recall, 'f1':f1, 'accuracy':accuracy}
+
+  result_df = result_df.round(2)
+
+  headers = {
+    "selector": "th:not(.index_name)",
+    "props": "background-color: #800000; color: white; text-align: center"
+  }
+  properties = {"border": "1px solid black", "width": "65px", "text-align": "center"}
+
+  fancy_df = result_df.style.format(precision=2).set_properties(**properties).set_table_styles([headers])
+  return (result_df, fancy_df)
       
       
